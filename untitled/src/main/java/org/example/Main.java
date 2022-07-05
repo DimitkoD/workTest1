ba5cde5845c1f890f2b8106a7cadb3778e0ef702
DimitkoD <84143276+DimitkoD@users.noreply.github.com>
2022-07-05 12:26:19 +0300

    no message

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
     }
 }
